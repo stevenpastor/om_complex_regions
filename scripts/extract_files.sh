e0784c65b80f9eccbc863686d67dd81ab5d266ff
wongjessica93 <jessikahh.wong@gmail.com>
2021-06-02 17:39:12 -0400

    Fixed bugs in scripts

--- scripts/extract_files.sh
+++ scripts/extract_files.sh
@@ -1,4 +1,6 @@
-SAMPLE=11029B
+#!/bin/bash
+
+SAMPLE=11029C
 ASSEMBLY=$SAMPLE'_Assembly_pipeline_results.tar.gz'
 
 ## For tar.gz assemblies
@@ -7,6 +9,6 @@ output/contigs/exp_refineFinal1_sv/merged_smaps/exp_refineFinal1_merged_q.cmap \
 output/contigs/exp_refineFinal1_sv/merged_smaps/exp_refineFinal1_merged_r.cmap \
 output/all.bnx.gz \
 output/molecules.tar.gz &&
-tar -xvf output/all.bnx.gz &&
+# tar -xvf output/all.bnx.gz &&
 tar -xvf output/molecules.tar.gz &&
 mv output data/$SAMPLE'_output'
